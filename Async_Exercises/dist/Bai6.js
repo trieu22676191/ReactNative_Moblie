cb523b61be15b82a7fa359d3c131fc43f4a37ddb
trieu22676191 <trieu22676191@gmail.com>
2025-08-30 14:08:58 +0700

    Lab2_Bai6

---
+++ Async_Exercises/dist/Bai6.js
@@ -0,0 +1,14 @@
+"use strict";
+function simulateTask1(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`Task with ${time}ms done`);
+        }, time);
+    });
+}
+const case1 = simulateTask1(400).then(() => "Case 1 done");
+const case2 = simulateTask1(600).then(() => "Case 2 done");
+const case3 = simulateTask1(200).then(() => "Case 3 done");
+Promise.all([case1, case2, case3]).then((results) => {
+    console.log("All cases done:", results);
+});
