b49f3392506f9791288d8c7d8ad8a761ad79ed16
trieu22676191 <trieu22676191@gmail.com>
2025-08-30 14:14:07 +0700

    Lab2_Bai7

---
+++ Async_Exercises/src/Bai7.ts
@@ -0,0 +1,5 @@
+const r1 = new Promise(resolve => setTimeout(() => resolve("r1"), 100));
+const r2 = new Promise(resolve => setTimeout(() => resolve("r2"), 300));
+const r3 = new Promise(resolve => setTimeout(() => resolve("r3"), 500));
+
+Promise.race([r1, r2, r3]).then(first => console.log("First:", first));
