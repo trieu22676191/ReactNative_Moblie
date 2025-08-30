096bf79ae9732d5a303031d6042e60aa53f479e4
trieu22676191 <trieu22676191@gmail.com>
2025-08-30 13:18:55 +0700

    Bai25

---
+++ OOP_Exercises/dist/Bai25.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Shape = void 0;
+class Shape {
+    static describe() {
+        return "A shape is a geometric figure such as a circle, square, or triangle.";
+    }
+}
+exports.Shape = Shape;
+console.log(Shape.describe());
---
+++ OOP_Exercises/src/Bai25.ts
@@ -0,0 +1,8 @@
+
+export class Shape {
+    static describe(): string {
+        return "A shape is a geometric figure such as a circle, square, or triangle.";
+    }
+}
+
+console.log(Shape.describe());
