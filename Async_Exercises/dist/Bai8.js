1a999b516bb6a22ec1316e3b00cf47cd0d925963
trieu22676191 <trieu22676191@gmail.com>
2025-08-30 14:24:24 +0700

    Lab2_Bai8

---
+++ Async_Exercises/dist/Bai8.js
@@ -0,0 +1,17 @@
+"use strict";
+Promise.resolve(2).then((n) => {
+    console.log("Giá trị ban đầu:", n);
+    const binh = n * n;
+    console.log("Sau khi bình phương:", binh);
+    return binh;
+}).then((n) => {
+    const x2 = n * 2;
+    console.log("Sau khi nhân 2:", x2);
+    return x2;
+}).then((n) => {
+    const cong = n + n;
+    console.log("Sau khi cộng:", cong);
+    return cong;
+}).then((res) => {
+    console.log("Kết quả cuối cùng:", res);
+});
