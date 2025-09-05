570cbb3675ba909618e9ac083d70ce1976d944f1
trieu22676191 <trieu22676191@gmail.com>
2025-09-05 21:25:54 +0700

    Lab2_Bai24

---
+++ Async_Exercises/src/Bai24.ts
@@ -0,0 +1,11 @@
+async function postData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: "foo", body: "bar", userId: 1 })
+  });
+  const data = await res.json();
+  console.log("Posted:", data);
+}
+
+postData();
