28b74d41832775de0f40b3cc470f46edad6ab91e
trieu22676191 <trieu22676191@gmail.com>
2025-09-05 21:01:11 +0700

    Lab2_Bai21

--- Async_Exercises/dist/Bai21.js
+++ Async_Exercises/dist/Bai21.js
@@ -1,6 +1,6 @@
 "use strict";
 async function getTodo() {
-    const res = await fetch("https://68baeb4e84055bce63f096c9.mockapi.io/Users");
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
     const data = await res.json();
     console.log(data);
 }
